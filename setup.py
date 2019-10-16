72b6cc0fdbc3cd53bcf191e1e5129b138562799f
Alex Sales <alexander.sales@equinox.com>
2019-10-16 21:08:15 +0800

    up version to 1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name='codb',
   packages=['codb'],
-  version=1.0,
+  version=1.1,
   license='MIT',
   description='common code for DBs',
   author='Equinox',
