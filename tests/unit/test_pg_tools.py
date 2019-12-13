a9e97da53dfd88c75b78ec3f3622840499f0a0ce
Alex Sales <alexander.sales@equinox.com>
2019-12-13 21:33:35 +0800

    Port datacoco3.db fix on jsonschema validation - add test - fix mssql test

--- tests/unit/test_pg_tools.py
+++ tests/unit/test_pg_tools.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import MagicMock
-
+import json
 from datacoco_db.pg_tools import PGInteraction
 
 
@@ -16,6 +16,26 @@ class TestPGInteraction(unittest.TestCase):
         )
         testClass.cur = MagicMock()
         testClass.bulk_dictionary_insert("table_name", {"key": "value"})
+
+    def test_validate_schema(self):
+        testClass = PGInteraction(
+            dbname="db_name",
+            host="host",
+            user="user",
+            password="password",
+            port="port",
+            schema="public",
+        )
+        schema = {
+            "type": "object",
+            "properties": {
+                "price": {"type": "number"},
+                "name": {"type": "string"},
+            },
+        }
+
+        testClass.cur = MagicMock()
+        testClass.validate_json_scheme(json.dumps({"name" : "Eggs", "price" : 34.99}), schema)
 
 
 if __name__ == "__main__":
